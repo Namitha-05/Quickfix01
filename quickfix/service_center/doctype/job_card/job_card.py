394e1fa543e4542534a3f6b8cee82770fe60f24d
Namitha <namithachinnasamy@gmail.com>
2026-02-24 23:11:02 +0530

    Doctypes Creation

---
+++ quickfix/service_center/doctype/job_card/job_card.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2026, Namitha and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+import frappe
+from frappe.model.document import Document
+
+class JobCard(Document):
+
+    def before_insert(self):
+        default_labor = frappe.db.get_single_value( "QuickFix Settings", "default_labour_charge")
+        if not self.labour_charge:
+            self.labour_charge = default_labor
---
+++ quickfix/service_center/doctype/spare_part/spare_part.py
@@ -0,0 +1,23 @@
+import frappe
+from frappe.model.document import Document
+from frappe.model.naming import make_autoname
+
+class SparePart(Document):
+
+    def autoname(self):
+        if self.part_code:
+        self.name = self.part_code.upper()+"-"+make_autoname("PART-.YYYY.-.####")
+
+
+    def validate(self):
+        self.validate_pricing()
+
+
+    def validate_pricing(self):
+        if self.unit_cost is not None and self.selling_price is not None:
+            if self.selling_price <= self.unit_cost:
+                frappe.throw(
+                    ("Selling Price ({0}) must be greater than Unit Cost ({1}).")
+                    .format(self.selling_price, self.unit_cost),
+                    title=("Invalid Pricing")
+                )
